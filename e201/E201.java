aa1ea5a81ae48f1b1c08297d3ec9b104f72ba11c
facucastellano19 <castellanofacundo05@gmail.com>
2024-06-26 00:13:40 -0300

    Add files via upload

---
+++ e201/E201.java
@@ -0,0 +1,26 @@
+
+package e201;
+
+
+public class E201 {
+
+
+    public static void main(String[] args) {
+        
+     //E201. Crear un algoritmo que escriba "Hola" cinco veces. Realizarlo de tres formas distintas: una usando un ciclo for, 
+     //la segunda usando un ciclo while, y la tercera usando un ciclo do...while. La salida deberá ser “Hola” 15 veces.
+            
+        hola h1 = new hola();
+
+        // Usar ciclo for
+        h1.holaFor();
+
+        // Usar ciclo while
+        h1.holaWhile();
+
+        // Usar ciclo do-while
+        h1.holaDoWhile();
+        
+    }
+    
+}
---
+++ e201/hola.java
@@ -0,0 +1,29 @@
+package e201;
+
+public class hola {
+    
+    public void holaFor() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hola");
+        }
+    }
+    
+    public void holaWhile() {
+        int contador = 0;
+        while (contador < 5) {
+            System.out.println("Hola");
+            contador++;
+        }
+    }
+    
+    public void holaDoWhile() {
+        int contador2 = 0;
+        do {
+            System.out.println("Hola");
+            contador2++;
+        } while (contador2 < 5);
+    }
+    
+    
+    
+}
